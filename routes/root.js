5e7cfd2c54d96a82462d7ec5babdecdefa6ed8a5
skillnter <skillhimanshubansal@gmail.com>
2021-10-10 00:13:27 +0530

    ADD ROUTE
    * Added route for endpoint '/:op_return'
    * Used getOpReturnDetails query function with op_return as param

--- routes/root.js
+++ routes/root.js
@@ -5,4 +5,26 @@ var router = express.Router();
 
 const query = require('../db/queries');
 
+/**
+ * @api {get} /:op_return Request OP_RETURN Hex
+ * @apiName GetTransactionAndBlockHash
+ * @apiGroup User
+ *
+ * @apiParam {String} op_return Hex of OP_RETURN.
+ *
+ * @apiSuccess {String} status Status Code.
+ * @apiSuccess {String|Object[]} data  Array of transaction and block hashes or Error Message.
+ */
+router.get('/:op_return', function (req, res) {
+	query.getOpReturnDetails(req.params['op_return']).then((data)=>{
+		let response = "No data found.";
+		if(data.rows.length > 0){
+			response = data.rows;
+		}
+		res.send({status:STATUS_CODES.OK,data:response});
+	}).catch((error)=>{
+		res.send({status:STATUS_CODES.INTERNAL_SERVER_ERROR,data:STATUS_CODES.getStatusDescription(STATUS_CODES.INTERNAL_SERVER_ERROR)});
+	});
+})
+
 module.exports = router;
